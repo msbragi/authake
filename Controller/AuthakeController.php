ceee215d1c3878efecb1cc58601434aa21dbd00c
Mutlu Tevfik Kocak <mtkocak@gmail.com>
2012-11-06 18:24:49 +0200

    prettified code

--- Controller/AuthakeController.php
+++ Controller/AuthakeController.php
@@ -1,33 +1,29 @@
 <?php
 /*
-    This file is part of Authake.
+	This file is part of Authake.
 
-    Author: Jérôme Combaz (jakecake/velay.greta.fr)
-    Contributors:
+	Author: Jérôme Combaz (jakecake/velay.greta.fr)
+	Contributors:
 
-    Authake is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	Authake is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    Authake is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Authake is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
 class AuthakeController extends AuthakeAppController {
-
-    var $uses = array();    // needed as we don't have any model
-    var $layout = 'authake';
-
-    function index() 
-    {
-        $this->set('title_for_layout', 'Authake User & Group Management');    
-    }
-
+	var $uses = array();// needed as we don't have any model
+	var $layout = 'authake';
+	
+	function index() {
+		$this->set('title_for_layout', 'Authake User & Group Management');
+	}
+}
 ?>
